71af09825fc2969c8a20da5c580ef004b975f8a4
Usman <ughani98@yahoo.com>
2024-09-16 14:43:50 +0500

    pushing the whole codel

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Interactive React App</h1>
+        <p>This is a simple interactive app deployed on ArgoCD!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Optional if you have a CSS file
+import App from './App'; // This assumes your main component is in App.js
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
